a0619159912d01a1386abfde2cdcf78824a44112
Fabio Sgambetterra <fabio.sgambetterra@accenture.com>
2023-11-25 13:06:44 +0100

    https

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EmailSchedulerService } from './emailScheduler/emailScheduler.services';
 import { config } from 'dotenv';
+import * as fs from 'fs';
 
 async function bootstrap() {
   config();
-  const app = await NestFactory.create(AppModule);
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH)
+    }
+  });
   await app.listen(process.env.APPLICATION_PORT);
 
   const emailSchedulerService = app.get(EmailSchedulerService);
